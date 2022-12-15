5d3b45fef6dc53d8b488a7c1428d9d62129c45c1
unknown <s.svetlana95@mail.ru>
2022-12-15 17:43:07 +0800

    The file has been changed

--- Task12.7.py
+++ Task12.7.py
@@ -17,7 +17,6 @@
 
 money = int(input('Введите сумму, которую планируете получить под проценты: '))
 per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
-# values_list = list(per_cent.values())
 
 deposit = []
 for i in per_cent.values():
