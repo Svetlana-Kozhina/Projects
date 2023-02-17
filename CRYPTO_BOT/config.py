c1a890d609bea0614b4fc463749437c49947dd8e
Svetlana-Kozhina <s.svetlana95@mail.ru>
2023-02-17 20:59:05 +0800

    New files have been added

---
+++ CRYPTO_BOT/config.py
@@ -0,0 +1,7 @@
+TOKEN = '5906147766:AAFtLnggvRcx_Ulh_O_WZgSTlcXxZ4uQlKA'
+
+keys = {
+    'биткоин': 'BTC',
+    'эфириум': 'ETH',
+    'доллар': 'USD',
+}
---
+++ CURRENCY_BOT/config.py
@@ -0,0 +1,7 @@
+TOKEN = '6241619561:AAEFHtXT5aEaYBGK93tO-fJyrVoiKhvW3Kc'
+
+keys = {
+    'евро': 'EUR',
+    'доллар': 'USD',
+    'рубль': 'RUB',
+}
